b211305439084344cb6ce03a43067c1631dea393
Pavel Hodakov <75166974+n3tw4lk3r@users.noreply.github.com>
2025-03-19 12:44:49 +0300

    Minor fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "SlotMachine.hpp"
 
-int main() {
+int main(int argc, char** argv) {
     const int width = 900;
     const int height = 600;
     SlotMachine slotMachine(width, height);
     slotMachine.play();
+    return 0;
 }
